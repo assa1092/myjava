64ed8b23a1f002f77cb99edf8ac4d1af9784f604
assa1092 <vincent1092@naver.com>
2020-03-30 10:11:31 +0900

    type casting

--- src/chap02/typecasting/Explitcit.java
+++ src/chap02/typecasting/Explitcit.java
@@ -7,8 +7,27 @@ public class Explitcit {
 		
 		int i = 103029770;
 		byte b = (byte) i;
+		
 		System.out.println(i);
-		System.out.println(b);
+		
+		// 4byte의 int가 1byte의 byte로 변환시 앞의 3byte는 0으로 채워진다
+		System.out.println(b); 
+		
+		long l=300;
+		i = (int)l ;
+		System.out.println(l);
+		System.out.println(i);
+		
+		int intValue  = 'A';
+		char charValue =  (char)intValue;
+		System.out.println(charValue);
+		
+		double d = 3.14;
+		int i3 = (int) d;	//소수점 이하는 버리고 정수로 
+		
+		System.out.println(i3);
+		
+		
 	}
 
 }
