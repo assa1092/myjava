121faeab45576e63ec67e183617befbd168ec2ee
assa1092 <vincent1092@naver.com>
2020-04-01 16:29:58 +0900

    switch

--- src/chap04/examples/IfDiceExample2.java
+++ src/chap04/examples/IfDiceExample2.java
@@ -5,13 +5,14 @@ public class IfDiceExample2 {
 		int num1 = (int)(Math.random()*6+1);
 		int num2 = (int)(Math.random()*6+1);
 		
+		System.out.println( num1 + " ," + num2);
 		if (num1==num2) {
-			System.out.println("num1 : " + num1);
-			System.out.println("num2 : " + num2);
+			
+			
 			System.out.println("당첨되었습니다");
 		} else {
-			System.out.println("num1 : " + num1);
-			System.out.println("num2 : " + num2);
+			
+			
 			System.out.println("당첨되지 않았습니다.");
 		}
 	}
