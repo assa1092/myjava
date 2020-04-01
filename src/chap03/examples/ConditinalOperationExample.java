5bd64b7c4a0361e5c208bad05156b51eebad9c79
assa1092 <vincent1092@naver.com>
2020-04-01 10:09:51 +0900

    bitwise, assignment,bitlogic,bitshift

--- src/chap03/examples/ConditinalOperationExample.java
+++ src/chap03/examples/ConditinalOperationExample.java
@@ -6,6 +6,8 @@ public class ConditinalOperationExample {
 		char grade = (score >90)? 'A' : ((score>80)? 'B': 'C') ;
 		System.out.println(score + " 점은" + grade + "등급입니다");
 		
+		
+		
 	}
 
 }
--- src/chap03/ternary/Conditinal.java
+++ src/chap03/ternary/Conditinal.java
@@ -4,11 +4,12 @@ public class Conditinal {
 	public static void main(String[] args) {
 		// 1항 ? 2항 : 3항 →2항 또는 3항
 		
-		// 1항이 true 이면 2항
-		// 1항이 false 이면 3항
+		// 1항값이 true 이면 2항
+		// 1항값이 false 이면 3항
 		
 		int i = (true ? 3 : 4);
 		System.out.println(i);
+		
 		i = (false ? 3 : 4);
 		System.out.println(i);
 	}
