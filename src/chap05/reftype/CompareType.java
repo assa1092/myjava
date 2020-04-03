bab0c20c7e0687c90f62e1ea8f8c9b73410f860c
assa1092 <vincent1092@naver.com>
2020-04-03 11:20:33 +0900

    New operator

--- src/chap05/reftype/CompareType.java
+++ src/chap05/reftype/CompareType.java
@@ -10,15 +10,18 @@ public class CompareType {
 		String s3 = new String("java");
 		
 		boolean b1 = s1 == s2;
-		System.out.println(b1);
+		System.out.println(b1);		// false
 		
 		b1 = s2 == s3;
-		System.out.println(b1);
+		// s2, s3의 주소값이 다르기때문에 
+		System.out.println(b1);		// false
 		
+
 		//s3의  주소값을 가져온다. 
 		s2 = s3;
+		// s2, s3의 주소값이 같으므로 true
 		b1 =  s2==s3;
-		System.out.println(b1);
+		System.out.println(b1);		// true
 		
 		
 	}
