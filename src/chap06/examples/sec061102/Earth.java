e700a0b6faaf2a8ece8582207c6596f8b1a7d601
assa1092 <vincent1092@naver.com>
2020-04-07 16:58:10 +0900

    codingbat-warmup01

--- src/chap06/examples/sec061102/Earth.java
+++ src/chap06/examples/sec061102/Earth.java
@@ -1,6 +1,8 @@
 package chap06.examples.sec061102;
 
 public class Earth {
+	
+	// Constance 상수
 	static final double EARTH_RADIUS = 6400;
 	static final double EARTH_SURFACE_AREA;
 	
--- src/codingbat/WarmUP1.java
+++ src/codingbat/WarmUP1.java
@@ -93,5 +93,155 @@ public class WarmUP1 {
 			}
 
 		}
+		
+		////////////////////////////////////////////////////
+		
+		public String front3(String str) {
+			if (str.length() > 3) {
+				String f3 = str.substring(0, 3);
+				return f3 + f3 + f3;
+			} else {
+				return str + str + str;
+			}
+
+		}
+
+		////////////////////////////////////////////////////
+		
+		public String backAround(String str) {
+			int len = str.length();
+			char last = str.charAt(len - 1);
+			return last + str + last;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public boolean or35(int n) {
+			boolean m3 = n % 3 == 0;
+			boolean m5 = n % 5 == 0;
+
+			return m3 || m5;
+		}
+
+
+		////////////////////////////////////////////////////
+		
+		public String front22(String str) {
+			String s2;
+
+			if (str.length() >= 2) {
+				s2 = str.substring(0, 2);
+			} else {
+				s2 = str;
+			}
+
+			return s2 + str + s2;
+		}
+		////////////////////////////////////////////////////
+		
+		public boolean startHi(String str) {
+			return str.startsWith("hi");
+		}
+
+		////////////////////////////////////////////////////
+		
+		public boolean icyHot(int temp1, int temp2) {
+			boolean negative = temp1 < 0 || temp2 < 0;
+			boolean over100 = temp1 > 100 || temp2 > 100;
+
+			return negative && over100;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public boolean in1020(int a, int b) {
+			boolean ain = a >= 10 && a <= 20;
+			boolean bin = b >= 10 && b <= 20;
+			return ain || bin;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public boolean hasTeen(int a, int b, int c) {
+			boolean ateen = a >= 13 && a <= 19;
+			boolean bteen = b >= 13 && b <= 19;
+			boolean cteen = c >= 13 && c <= 19;
+
+			return ateen || bteen || cteen;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public boolean loneTeen(int a, int b) {
+			boolean ateen = a >= 13 && a <= 19;
+			boolean bteen = b >= 13 && b <= 19;
+
+			return ateen ^ bteen;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public String delDel(String str) {
+			if (str.indexOf("del") == 1) {
+				String left = str.substring(0, 1);
+				String right = str.substring(4);
+				return left + right;
+			}
+			return str;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public boolean mixStart(String str) {
+			return str.indexOf("ix") == 1;
+		}
+
+		////////////////////////////////////////////////////
+		
+		public String startOz(String str) {
+			int loop = 2 < str.length() ? 2 : str.length();
+			String r = "";
+			for (int i = 0; i < loop; i++) {
+
+				if (i == 0 && str.charAt(i) == 'o') {
+					r += str.charAt(i);
+				}
+
+				if (i == 1 && str.charAt(i) == 'z') {
+					r += str.charAt(i);
+				}
+			}
+			return r;
+		}
+		
+		////////////////////////////////////////////////////
+
+		public int intMax(int a, int b, int c) {
+			int r = a < b ? b : a;
+			r = r < c ? c : r;
+			return r;
+		}
+
+		
+		////////////////////////////////////////////////////
+		
+		public int close10(int a, int b) {
+			int adiff = Math.abs(a - 10);
+			int bdiff = Math.abs(b - 10);
+
+			int r = adiff > bdiff ? b : a;
+
+			r = adiff == bdiff ? 0 : r;
+			return r;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 }
