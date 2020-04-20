ee13c637e2e78d3c420aee7eaf7a94b7bbd8e97e
assa1092 <vincent1092@naver.com>
2020-04-20 15:43:04 +0900

    chap08-exercises

--- src/chap08/examples/sec080504/Bus.java
+++ src/chap08/examples/sec080504/Bus.java
@@ -2,14 +2,15 @@ package chap08.examples.sec080504;
 
 public class Bus implements Vehicle {
 	
-	Bus() {
-		System.out.println("버스가 생성됨");
-		
-	}
+	
 	@Override
 	public void run() {
 		System.out.println("버스가 달립니다.");
 		
+	}
+	
+	public void checkFare() {
+		System.out.println("승차요금을 체크합니다..");
 	}
 
 }
