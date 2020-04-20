3c1f534ff8ff354a0aad9a2cf16778e5bb1542e5
assa1092 <vincent1092@naver.com>
2020-04-20 16:46:13 +0900

    Interface - inhehitance

--- src/chap08/exercises/Ex05/ActionExample.java
+++ src/chap08/exercises/Ex05/ActionExample.java
@@ -3,6 +3,10 @@ package chap08.exercises.Ex05;
 public class ActionExample {
 	
 	public static void main(String[] args) {
+		
+		// 보통 클래스객체를 만들고 그 객체에서 접근을 하지만
+		
+		// 일회용으로 한번만 실행할 시에는  익명구현 객체를 만든다.
 		Action action = new Action(){
 			@Override
 			public void work() {
