0d98d23bccc10d803d26bf402c8fea25ea7c8f99
assa1092 <vincent1092@naver.com>
2020-04-22 09:01:05 +0900

    exercises

--- src/chap09/examples/Ex06/Chatting.java
+++ src/chap09/examples/Ex06/Chatting.java
@@ -2,15 +2,14 @@ package chap09.examples.Ex06;
 
 public class Chatting {
 	void startChat(String chatId) {
-		String nickName = null;
-		nickName = chatId;
+		String nickName = chatId;
 		Chat chat = new Chat() {
 			@Override
 			public void start() {
 				while(true) {
 					String inputData = "안녕하세요.";
-//					String message = "["+ nickName + "]" + inputData;
-//					sendMessage(message);
+					String message = "["+ nickName + "]" + inputData;
+					sendMessage(message);
 				}
 				
 			}
