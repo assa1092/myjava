cd56cbda28e2507c761f45861a4a2ead51221c09
assa1092 <vincent1092@naver.com>
2020-04-22 17:23:56 +0900

    사용자정의 예외 처리

--- src/chap10/examples/sec100701/AccountExample.java
+++ src/chap10/examples/sec100701/AccountExample.java
@@ -11,6 +11,7 @@ public class AccountExample {
 		// 출금하기
 		try {
 			account.withdraw(30000);
+			System.out.println("예금액: " + account.getBalance());
 		} catch (BalanceInsufficientException e) {
 			String message = e.getMessage();
 			System.out.println(message);
