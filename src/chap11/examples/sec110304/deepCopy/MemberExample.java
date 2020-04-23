0350a11b5a6f3a5806019e8a70d4864960d84485
assa1092 <vincent1092@naver.com>
2020-04-23 16:56:40 +0900

    compare

--- src/chap11/examples/sec110304/deepCopy/MemberExample.java
+++ src/chap11/examples/sec110304/deepCopy/MemberExample.java
@@ -3,11 +3,15 @@ package chap11.examples.sec110304.deepCopy;
 public class MemberExample {
 	public static void main(String[] args) {
 		
+		// 원본 객체 생성
 		Member original = new Member("홍길동", 23, new int[] {90,90}, new Car("소나타"));
 		
+		// 복제 객체를 얻은후에 참조 객체의 값을 변경
 		Member cloned = original.getMember();
-		cloned.scores[0]=100;
+		cloned.scores[0]=100;			// 깊은 복제 후 참조객체의 데이터를 변경
 		cloned.car.model="그랜져";
+		
+		
 		
 		System.out.println("[복제 객체의 필드값]");
 		System.out.println("name: " + cloned.name);
@@ -23,7 +27,7 @@ public class MemberExample {
 		
 		System.out.println();
 		
-		System.out.println("[복제 객체의 필드값]");
+		System.out.println("[원본 객체의 필드값]");
 		System.out.println("name: " + original.name);
 		System.out.println("age:" + original.age);
 		System.out.print("scores: {");
